a4c3c676d9699bf7f9fd788610e30bcf5687e98f
Kenneth G Bastian <kenneth.g.bastian@descouvre.com>
2017-07-23 05:41:57 +0800

    added color to table

--- modules/modelfaux.js
+++ modules/modelfaux.js
@@ -4,6 +4,7 @@
 const trainingData = require('../training');
 const categories = ['critical', 'major', 'minor'];
 let allWords = [];
+let set = new Set();
 let wordCategories = {};
 
 categories.forEach((category) => {
@@ -19,6 +20,9 @@ categories.forEach((category) => {
 
     //- track all words for feature distribution
     allWords = allWords.concat(words);
+
+    //- track all unique words
+    words.forEach(w => { set.add(w); });
 
     //- track words by category
     wordCategories[category] = wordCategories[category].concat(words);
@@ -45,16 +49,24 @@ function extractFeatures(words) {
 
 function classify(message) {
   let score = {};
+
+  const words = extractFeatures(message.toLowerCase()
+    .replace(/[^a-z0-9 ]/gm, '')
+    .split(' ')
+    .filter(w => w.length > 0));
+
   categories.forEach(function(category) {
     const classCount = Object.keys(model[category]).length; // count(c)
-    const vocabularyCount = allWords.length; // |V|
+    const vocabularyCount = set.size; // |V|
     score[category] = {};
     score[category].value = 1;
 
     const features = model[category];
     for(let property in features) {
       if(features.hasOwnProperty(property)) {
-        score[category].value *= (features[property] + 1) / (classCount + vocabularyCount + 1);
+        let featFreqPerClass = 0;
+        if(words[property]) { featFreqPerClass = features[property]; }
+        score[category].value *= (featFreqPerClass + 1) / (classCount + vocabularyCount + 1);
       }
     }
   });
@@ -72,18 +84,31 @@ function classify(message) {
   return selectedClass;
 }
 
-// module.exports = {
-//   classify
-// };
+const bayes = require('bayes');
+const classifier = bayes();
+
+categories.forEach((category) => {
+  const messages = trainingData[category].messages;
+  // const coords = trainingData[category].coords;
+
+  for(let i = 0; i < messages.length; ++i) {
+    classifier.learn(messages[i], category);
+  }
+});
 
 const msgs = [
-  'hello po',
-  'kamusta kayo diyan ito bago kong roaming number',
+  // 'hello po',
+  // 'kamusta kayo diyan ito bago kong roaming number',
   'good morning sa inyo diyan',
-  'ano gawa mo bes?',
-  'kritical lagay ngayon ni tito, punta ka dali'
+  // 'ano gawa mo bes?',
+  // 'kritical lagay ngayon ni tito, punta ka dali',
+  'bumagsak yung bldg dito sa 32nd st. nid rescue asap',
+  // 'sulit sa kain dito pre',
+  // 'lintik lang ang walang ganti',
+  // 'oi gago ka, magpakita ka sa akin'
 ];
 msgs.forEach(msg => {
   const res = classify(msg);
-  console.log(msg, res);
+  const ccc = classifier.categorize(msg);
+  console.log(msg, res, ccc);
 });
