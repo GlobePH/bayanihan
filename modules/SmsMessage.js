3752bfb5d25dc5a2123f1a0af3c92af47c11e0a3
mens-rea <juan.miguel.g.alvarez@gmail.com>
2017-07-23 09:23:55 +0800

    added name to table details for representation of test case

--- modules/SmsMessage.js
+++ modules/SmsMessage.js
@@ -8,7 +8,7 @@ class SmsMessage {
   }
 
   get raw() {
-    return this.timestamp + ';' + this.mobileNumber + ';' + this.category + ';' + this.message;
+    return this.timestamp + ';' + this.mobileNumber + ';' + this.category + ';' + this.message + ';' + this.name;
   }
 
   static mapFromCache(item) {
@@ -18,6 +18,7 @@ class SmsMessage {
     msg.mobileNumber = splits[1];
     msg.category = splits[2];
     msg.message = splits[3];
+    msg.name = splits[4];
     msg.friendlyDate = moment(msg.timestamp).format('MMM DD, YYYY HH:mm:ss');
     return msg;
   }
@@ -25,6 +26,9 @@ class SmsMessage {
   static mapFromInbound(im) {
     const smg = new SmsMessage();
     smg.mobileNumber = im.senderAddress.replace('tel:', '');
+    if(smg.mobileNumber=="+639278854260"){ smg.name = "Allysa Marie"; }
+    else if(smg.mobileNumber=="+639954889586"){ smg.name = "Mikko Dimapasoc"; }
+    else { smg.name = "Unknown"; }
     smg.message = (im.message || '').toLowerCase();
     smg.timestamp = moment().toISOString();
     smg.friendlyDate = moment(smg.timestamp).format('MMM DD, YYYY HH:mm:ss');
